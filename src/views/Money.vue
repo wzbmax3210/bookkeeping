33b297c07041d4aa7313f41834055d2916e5c8d6
max <1127979416@qq.com>
2020-03-26 20:13:21 +0800

    以typescript的方式完成 Tags.vue Money.vue的script改成ts的表达方式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,20 +3,24 @@
   <NumberPad></NumberPad>
   <Types :xxx="111"></Types>
   <Notes></Notes>
-  <Tags></Tags>
+  <Tags :data-source.sync="tags"></Tags>
 </Layout>
 
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
 import NumberPad from '@/components/Money/NumberPad.vue'
 import Types from '@/components/Money/Types.vue'
 import Notes from '@/components/Money/Notes.vue'
 import Tags from '@/components/Money/Tags.vue'
+import {Component} from 'vue-property-decorator'
 
-export default {
-  name: 'Money',
-  components: {Tags, Notes, Types, NumberPad}
+@Component({
+  components: {Tags, Notes, Types, NumberPad},
+})
+export default class Money extends Vue{
+  tags = ['衣', '食', '住', '行']
 }
 </script>
 
