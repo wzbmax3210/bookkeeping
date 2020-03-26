9aef8ec88c1923ad6d51086a07822a81579bd55d
max <1127979416@qq.com>
2020-03-26 15:42:12 +0800

    将Money.vue 的内容拆分成子组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,59 +1,22 @@
 <template>
 <Layout class-prefix="layout">
-  <div class="numberPad">
-    <div class="output">100</div>
-    <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <!--<button>+</button>-->
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <!--<button>-</button>-->
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <!--<button>*</button>-->
-      <button class="ok">ok</button>
-      <button class="zero">0</button>
-      <button>.</button>
-      <!--<button>%</button>
-      <button>➗</button>-->
-    </div>
-  </div>
-  <div>
-    <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
-    </ul>
-  </div>
-  <div>
-    <label class="notes">
-      <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入备注">
-    </label>
-  </div>
-  <div class="tags">
-    <div class="new">
-      <button>新增标签</button>
-    </div>
-    <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-    </ul>
-  </div>
+  <NumberPad></NumberPad>
+  <Types></Types>
+  <Notes></Notes>
+  <Tags></Tags>
 </Layout>
 
 </template>
 
 <script lang="ts">
+import NumberPad from '@/components/Money/NumberPad.vue'
+import Types from '@/components/Money/Types.vue'
+import Notes from '@/components/Money/Notes.vue'
+import Tags from '@/components/Money/Tags.vue'
+
 export default {
-  name: 'Money'
+  name: 'Money',
+  components: {Tags, Notes, Types, NumberPad}
 }
 </script>
 
@@ -65,128 +28,4 @@ export default {
 </style>
 <style scoped lang="scss">
   @import "~@/assets/style/helper.scss";
-  .numberPad {
-    .output {
-      @extend %clearFix;
-      @extend %innerShadow;
-      font-size: 36px;
-      font-family: Consolas, monospace;
-      padding: 9px 16px;
-      text-align: right;
-    }
-    .buttons {
-      @extend %clearFix;
-      > button {
-        width: 25%;
-        height: 64px;
-        border: none;
-        background: transparent;
-        &.ok {
-          float: right;
-          height: (64px*2);
-        }
-        &.zero {
-          width: 25*2%;
-        }
-        $bg: #f2f2f2;
-        &:nth-child(1) {
-          background: $bg;
-        }
-        &:nth-child(2), &:nth-child(5) {
-          background: darken($bg, 4%);
-        }
-        &:nth-child(3), &:nth-child(6),  &:nth-child(9){
-          background: darken($bg, 4*2%);
-        }
-        &:nth-child(4), &:nth-child(7),  &:nth-child(10){
-          background: darken($bg, 4*3%);
-        }
-        &:nth-child(8), &:nth-child(11),  &:nth-child(13){
-          background: darken($bg, 4*4%);
-        }
-        &:nth-child(14) {
-          background: darken($bg, 4*5%);
-        }
-        &:nth-child(12) {
-          background: darken($bg, 4*6%);
-        }
-      }
-    }
-  }
-
-  .types {
-    background: #c4c4c4;
-    display: flex;
-    text-align: center;
-    font-size: 24px;
-    > li {
-      width: 50%;
-      height: 64px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-      &.selected::after {
-        content: '';
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 4px;
-        background: #333333;
-      }
-    }
-  }
-
-  .notes {
-    background: #f5f5f5;
-    font-size: 14px;
-    padding-left: 16px;
-    display: flex;
-    align-items: center;
-
-    .name {
-      padding-right: 16px;
-    }
-
-    input {
-      height: 64px;
-      flex-grow: 1;
-      background: transparent;
-      border: none;
-      padding-right: 16px;
-    }
-  }
-
-  .tags {
-    flex-grow: 1;
-    font-size: 14px;
-    padding: 16px;
-    display: flex;
-    flex-direction: column-reverse;
-    > .current {
-      display: flex;
-      flex-wrap: wrap;
-      > li {
-        background: #d9d9d9;
-        height: 24px;
-        line-height: 24px;
-        border-radius: (24px/2);
-        padding: 0 16px;
-        margin-right: 12px;
-      }
-    }
-
-    > .new {
-      padding-top: 16px;
-
-      button {
-        background: transparent;
-        border: none;
-        color: #999999;
-        border-bottom: 1px solid;
-        padding: 0 4px;
-      }
-    }
-  }
 </style>
