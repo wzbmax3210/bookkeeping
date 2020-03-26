7a0314371b46afca7cd63045b66541e7052b51f4
max <1127979416@qq.com>
2020-03-26 15:30:14 +0800

    complete Money.vue html & css

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
   @import "~@/assets/style/reset.scss";
   @import "~@/assets/style/helper.scss";
   body {
+    font-size: 16px;
     font-family: $font-hei;
     line-height: 1.5;
     color: #333333;
